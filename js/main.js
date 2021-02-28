50d517af847e3a9b7fd414e7f1277c0aa9d0541f
ryosuke-horie <h29899958@icloud.com>
2021-03-01 00:17:35 +0900

    残り時間を表示

--- js/main.js
+++ js/main.js
@@ -1,28 +1,40 @@
-(function() {
+(function () {
     'use strict';
-  
+
     var timer = document.getElementById('timer');
     var min = document.getElementById('min');
     var sec = document.getElementById('sec');
     var reset = document.getElementById('reset');
     var start = document.getElementById('start');
-  
+
     var startTime;
     var timeLeft;
     var timeToCountDown = 4 * 1000;
-  
-    function countDown() {
-      setTimeout(function() {
-        // var elapsedTime = Date.now() - startTime;
-        // timeLeft = timeToCountDown - elapsedTime;
-        timeLeft = timeToCountDown - (Date.now() - startTime);
-        console.log(timeLeft);
-        countDown();
-      }, 10);
+
+    function updateTimer(t) {
+        var d = new  Date(t);
+        var m = d.getMinutes();
+        var s = d.getSeconds();
+        var ms = d.getMilliseconds();
+        m = ('0' + m).slice(-2);
+        s = ('0' + s).slice(-2);
+        ms = ('00' + ms).slice(-3);
+        timer.textContent = m + ':' + s + '.' + ms;
     }
-  
-    start.addEventListener('click', function() {
-      startTime = Date.now();
-      countDown();
+
+    function countDown() {
+        setTimeout(function () {
+            // var elapsedTime = Date.now() - startTime;
+            // timeLeft = timeToCountDown - elapsedTime;
+            timeLeft = timeToCountDown - (Date.now() - startTime);
+            // console.log(timeLeft);
+            updateTimer(timeLeft);
+            countDown();
+        }, 10);
+    }
+
+    start.addEventListener('click', function () {
+        startTime = Date.now();
+        countDown();
     });
-  })();
+})();
